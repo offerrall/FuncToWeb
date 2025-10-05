dbc37e680ea42c9540ceb3df16ba3371b39f0f0f
offerrall <offerrallps4@gmail.com>
2025-10-05 22:03:00 +0200

    aa

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="functoweb",
+    version="0.1.0",
+    author="Beltrán Offerrall",
+    packages=find_packages(),
+    package_data={'FuncToWeb': ['templates/*.html']},
+    install_requires=[
+        'fastapi',
+        'uvicorn',
+        'pydantic',
+        'jinja2',
+        'python-multipart',
+    ],
+)
