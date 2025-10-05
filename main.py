8488e3628d64866b45cc93831d6384ace6bfbae2
offerrall <offerrallps4@gmail.com>
2025-10-05 12:14:56 +0200

    aa

--- main.py
+++ main.py
@@ -4,12 +4,11 @@ from datetime import date, time
 from FuncToWeb import run, Color, Email
 
 def create_profile(
-    primary_color: Color,
     name: Annotated[str, Field(min_length=3, max_length=50)] = "John Doe",
     email: Email = "user@example.com",
     event_hour: time = time(14, 30),
     birth_date: date = date(1990, 1, 1),
-    secondary_color: Color = "#10b981",
+    color: Color = "#10b981",
     age: Annotated[int, Field(ge=18, le=120)] = 25,
     theme: Literal['light', 'dark', 'auto'] = 'auto',
     newsletter: bool = True
@@ -19,10 +18,7 @@ def create_profile(
         "email": email,
         "birth_date": birth_date.isoformat(),
         "event_hour": event_hour.isoformat(),
-        "colors": {
-            "primary": primary_color,
-            "secondary": secondary_color
-        },
+        "colors": color,
         "age": age,
         "theme": theme,
         "newsletter": newsletter
