b57d25bf5a717d8a4ecdf4ba88b6ba59cb3010f5
offerrall <offerrallps4@gmail.com>
2025-10-05 11:34:26 +0200

    aa

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from bbb import run, UI, Limits, Selected, Color, Email
 
 
 def create_profile(
+    primary_color: Color,
     name: UI[str, Limits(min_length=3, max_length=50)] = "John Doe",
     email: Email = "user@example.com",
-    primary_color: Color = "#4f46e5",
     secondary_color: Color = "#10b981",
     age: UI[int, Limits(ge=18, le=120)] = 25,
     theme: Selected['light', 'dark', 'auto'] = 'auto',
