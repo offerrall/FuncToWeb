1c6ac09ec763c48f3c1f2cf8eea3a5812211444a
offerrall <offerrallps4@gmail.com>
2025-10-05 07:45:19 +0200

    aa

--- all_types.py
+++ all_types.py
@@ -2,11 +2,13 @@
 from FuncToWeb import run
 from FuncToWeb.ui_types import IntUi, StrUi, FloatUi, BoolUi
 
-def FuncToWeb(int_param: IntUi(min=1, max=10),
-              list_str_param: list[str] = ["Option 1", "Option 2", "Option 3"],
-              str_param: StrUi(min_length=3, max_length=20) = "Hello",
-              float_param: FloatUi(min=0.5, max=99.9) = 1.5,
-              bool_param: BoolUi() = False):
+def FuncToWeb(
+    int_param: IntUi(min=1, max=10),
+    list_str_param: list[str] = ["Option 1", "Option 2", "Option 3"],
+    str_param: StrUi(min_length=3, max_length=20) = "Hello",
+    float_param: FloatUi(min=0.5, max=99.9) = 1.5,
+    bool_param: BoolUi() = False
+):
     
     return f"Int: {int_param}, {type(int_param)}; " \
            f"List Str: {list_str_param}, {type(list_str_param)}; " \
