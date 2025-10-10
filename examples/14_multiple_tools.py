2c0ec443390b0dc34b1db36d790c5eb73bd4a2f0
offerrall <offerrallps4@gmail.com>
2025-10-10 11:28:11 +0200

    Fix default value for celsius_to_fahrenheit function to use float type

--- examples/14_multiple_tools.py
+++ examples/14_multiple_tools.py
@@ -26,7 +26,7 @@ def calculate_bmi(
     }
 
 
-def celsius_to_fahrenheit(celsius: float = 0):
+def celsius_to_fahrenheit(celsius: float = 0.0):
     fahrenheit = (celsius * 9/5) + 32
     return f"{celsius}°C = {fahrenheit}°F"
 
